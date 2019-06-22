0a0a6f5aa759c27c90e0ba7dbd3da8f7e1f936c9
HaneetSingh <Haneet555@gmail.com>
2019-06-22 16:08:12 +0530

    humne

--- app/src/main/java/com/haneet/wheelwidget/view/LTSpinningWheelView.java
+++ app/src/main/java/com/haneet/wheelwidget/view/LTSpinningWheelView.java
@@ -562,7 +562,7 @@ public class LTSpinningWheelView extends View implements WheelRotation.RotationL
 
         canvas.save();
         canvas.rotate(angle, cx, cy);
-        Drawable d = ContextCompat.getDrawable(getContext(), R.drawable.top_car_rotate_obj);
+        Drawable d = ContextCompat.getDrawable(getContext(), R.drawable.disc_top);
         d.setBounds((int) left, (int) top, (int) right, (int) bottom);
         d.draw(canvas);
         //canvas.drawArc(rectF, 0, getAnglePerItem(), true, getItemPaint(i));
